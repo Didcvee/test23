beb35b75cd972c5c0d4d3b092a4c03276ecf01ef
Didcvee <8anibus8@gmail.com>
2024-06-26 13:01:34 +0300

    first

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+<br />
+<div align="center">
+  <a href="https://github.com/othneildrew/Best-README-Template">
+    <img src="images/img.png" alt="Logo" width="80" height="80">
+  </a>
+
+<h3 align="center">Test</h3>
+</div>
+
+Для запуска:
+* .\gradlew clean build
+* java -jar {root}/build/libs/[parsedata-1.0-SNAPSHOT.jar](build%2Flibs%2Fparsedata-1.0-SNAPSHOT.jar) {ваше расположение файла lng.txt}
+
+Время работы - 5 секунд
+
+
+
---
+++ src/main/java/ru/didcvee/Main.java
@@ -0,0 +1,185 @@
+package ru.didcvee;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+
+public class Main {
+
+    static volatile boolean check = true;
+    static Set<Long[]> mainFilteredSet = new LinkedHashSet<>();
+    static Set<Map<Long, Set<Long[]>>> result = new LinkedHashSet<>();
+    static int maxAmountElementsOfLine = 0;
+    static String pathToFile = "";
+    static final String regex = "^(\"\\d*\")(;\"\\d*\")*$";
+    static final Pattern pattern = Pattern.compile(regex);
+
+    public static void main(String[] args) {
+        pathToFile = args[0];
+        Runnable task = new Runnable() {
+            int countTime = 0;
+            public void run() {
+                while (check) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    System.out.println(countTime++);
+                }
+            }
+        };
+
+        LocalTime startTime = LocalTime.now();
+
+        Thread myThread = new Thread(task);
+        myThread.start();
+
+        System.out.println("начало");
+        Set<Long[]> copyOfMainSet = readFile(pathToFile, pattern);
+        if (!copyOfMainSet.isEmpty()) {
+            findMaxAmount();
+            Set<Map<Long, Set<Long[]>>> forTest = findMatches(copyOfMainSet, maxAmountElementsOfLine);
+            print(forTest);
+        } else {
+            System.out.println("ошибка");
+        }
+        check = false;
+        long secondsOfWork = ChronoUnit.SECONDS.between(startTime, LocalTime.now());
+        System.out.printf("время работы: %d сек \n", secondsOfWork);
+    }
+
+    public static Set<Long[]> readFile(String pathToFile, Pattern pattern) {
+        Set<Long[]> set = null;
+        try (Stream<String> lines = Files.lines(Paths.get(pathToFile))) {
+            set = lines.filter(s -> {
+                        Matcher matcher = pattern.matcher(s);
+                        return matcher.matches();
+                    })
+                    .filter(s -> (s.length() > 2))
+                    .distinct()
+                    .map(s -> s.replace("\"\"", "0"))
+                    .map(s -> s.replace("\"", ""))
+                    .map(s -> Arrays.stream(s.split(";"))
+                            .map(Long::valueOf)
+                            .toList()
+                            .toArray(Long[]::new))
+                    .collect(Collectors.toSet());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("ошибка");
+        }
+
+        if (set != null && !set.isEmpty()) {
+            mainFilteredSet.addAll(set);
+        }
+
+        return new LinkedHashSet<>(mainFilteredSet);
+    }
+
+    private static void findMaxAmount() {
+        for (Long[] x : mainFilteredSet) {
+            if (maxAmountElementsOfLine <= x.length)
+                maxAmountElementsOfLine = x.length;
+        }
+    }
+
+    public static Set<Map<Long, Set<Long[]>>> findMatches(Set<Long[]> copyOfMainSet, int maxElements) {
+        int size = 1;
+        Set<Map<Long, Set<Long[]>>> setOfMatchesPerColumn = new LinkedHashSet<>();
+        while (maxElements >= size) {
+
+            int finalSize = size;
+            long[] thumb = copyOfMainSet.stream()
+                    .flatMapToLong(s -> LongStream.of(Arrays.stream(s)
+                            .skip(finalSize - 1)
+                            .findFirst()
+                            .orElse(0L)))
+                    .toArray();
+
+            Set<Long> clear = new HashSet<>();
+            Set<Long> copies = new HashSet<>();
+            for (long l : thumb) {
+                if (l != 0 && !clear.add(l)) {
+                    copies.add(l);
+                }
+            }
+
+            Map<Long, Set<Long[]>> mapWithMatches = copyOfMainSet.stream()
+                    .filter(s -> Arrays.stream(s)
+                            .skip(finalSize - 1)
+                            .limit(1)
+                            .anyMatch(copies::contains))
+                    .collect(Collectors.groupingBy(
+                            s -> Arrays.stream(s)
+                                    .skip(finalSize - 1)
+                                    .limit(1)
+                                    .findFirst().orElse(0L),
+                            Collectors.toSet()
+                    ));
+
+            if (!mapWithMatches.isEmpty()) {
+                result.add(mapWithMatches);
+                setOfMatchesPerColumn.add(mapWithMatches);
+            }
+            size++;
+        }
+        return setOfMatchesPerColumn;
+    }
+
+    public static void print(Set<Map<Long, Set<Long[]>>> setOfDuplicatesInMaps) {
+
+        String newFileName = pathToFile.replace(".txt", "-result.txt");
+        Path outputPath = Path.of(newFileName);
+        if (!Files.exists(outputPath)) {
+            try {
+                Files.createFile(outputPath);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                System.out.println("ошибка");
+            }
+        }
+
+        try (FileOutputStream fos = new FileOutputStream(outputPath.toFile());
+             PrintStream out = new PrintStream(fos)) {
+            out.println(takeQuantityOfGroups());
+            setOfDuplicatesInMaps.forEach(m -> {
+                for (Map.Entry<Long, Set<Long[]>> longs : m.entrySet()) {
+                    out.printf("группа %d\n", longs.getKey());
+                    longs.getValue().forEach(s -> out.println(Arrays.toString(s)));
+                }
+            });
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            System.out.println("файл не найден");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("ошибка записи в файл");
+        }
+    }
+
+    private static int takeQuantityOfGroups() {
+        int quantityOfGroups = 0;
+        for (Map<Long, Set<Long[]>> x : result) {
+            quantityOfGroups += x.keySet().size();
+        }
+        return quantityOfGroups;
+    }
+}
